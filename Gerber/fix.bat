be56c4c0316844e096695cfcea6a2d8af11d85b4
Randy Stockberger <randystockberger@minetfiber.com>
2015-02-02 17:37:40 -0800

    Initial version of tortoise relay panel in 4-up configuration

---
+++ Gerber/fix.bat
@@ -0,0 +1,8 @@
+rename Tortoise-Relay-4up-B_Cu.gbl      Tortoise-Relay-4up.gbl
+rename Tortoise-Relay-4up-B_Mask.gbs    Tortoise-Relay-4up.gbs
+rename Tortoise-Relay-4up-B_SilkS.gbo   Tortoise-Relay-4up.gbo
+rename Tortoise-Relay-4up-Edge_Cuts.gbr Tortoise-Relay-4up.gbr
+rename Tortoise-Relay-4up-F_Cu.gtl      Tortoise-Relay-4up.gtl
+rename Tortoise-Relay-4up-F_Mask.gts    Tortoise-Relay-4up.gts
+rename Tortoise-Relay-4up-F_SilkS.gto   Tortoise-Relay-4up.gto
+rename Tortoise-Relay-4up.drl           Tortoise-Relay-4up.drl
